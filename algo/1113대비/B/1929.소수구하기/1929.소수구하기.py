61a4a1187ac28cace0d5f810cc1ecee95ac12851
diharong <diharong01@gmail.com>
2024-11-05 20:45:46 +0900

    notebook

--- algo/1113대비/B/1929.소수구하기/1929.소수구하기.py
+++ algo/1113대비/B/1929.소수구하기/1929.소수구하기.py
@@ -1,18 +1,26 @@
-import sys
-sys.stdin = open('input.txt')
+# import sys
+# sys.stdin = open('input.txt')
 
-M, N = map(int,input().split())
+# M, N = map(int,input().split())
+#
+# so_su = []  # 소수인 수를 받을 빈 리스트 만들기
+#
+# for num in range(M, N+1):
+#     if M < 2 :  # 1은 소수가 아니므로 제외시키는 조건문 달아주기
+#         continue
+#
+#     for i in range(2, num):
+#         if num%i == 0:  # 나누어 떨어지는 수가 있으면 소수 아님
+#             break
+#     else:
+#         so_su.append(num)
+#
+# print(*so_su, sep='\n')
 
-so_su = []  # 소수인 수를 받을 빈 리스트 만들기
+T = int(input())
+for tc in range(T):
+    N_arr = int(input())
 
-for num in range(M, N+1):
-    if M < 2 :  # 1은 소수가 아니므로 제외시키는 조건문 달아주기
-        continue
+    sorted(N_arr)
 
-    for i in range(2, num):
-        if num%i == 0:  # 나누어 떨어지는 수가 있으면 소수 아님
-            break
-    else:
-        so_su.append(num)
-
-print(*so_su, sep='\n')
+print(N_arr)
